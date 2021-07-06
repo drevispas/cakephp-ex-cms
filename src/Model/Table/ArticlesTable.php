a977a58e4eadc5120c0a90ad83faad0375f76d22
yong <yong@chai.finance>
2021-07-06 10:25:18 +0900

    feat: Create add() for ArticlesController.

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -6,7 +6,9 @@ namespace App\Model\Table;
 
 // By convention, table name will be `articles`.
 // `id` column is the primary key.
+use Cake\Event\EventInterface;
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
@@ -14,5 +16,15 @@ class ArticlesTable extends Table
     {
         // Populate created and modified columns.
         $this->addBehavior('Timestamp');
+    }
+
+    // a callbak to update `slug` column before saving.
+    public function beforeSave(EventInterface $event,$entity,$options){
+        // if `slug` value is null
+        if($entity->isNew() && !$entity->slug){
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug=substr($sluggedTitle,0,191);
+        }
     }
 }
